1ba695381a1a84b0a1a3870933ea5efa8cca5a8f
Liehan Els <ST10085345@vcconnect.edu.za>
2024-10-21 22:18:39 +0200

    Added a timer

--- WolfGame/Form1.cs
+++ WolfGame/Form1.cs
@@ -4,11 +4,167 @@ using System.Windows.Forms;
 
 namespace WolfGame
 {
+    public partial class MainForm : Form
+    {
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Timer countdownTimer;
+        private Wolf playerWolf;
+        private Item item;
+        private Wall[] walls;
+        private Random random;
+        private int score;
+        private int timeLeft;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.ClientSize = new Size(800, 600);
+
+            playerWolf = new Wolf("Alpha", 30, new Point(400, 300));
+            random = new Random();
+            item = new Item(new Point(random.Next(ClientSize.Width), random.Next(ClientSize.Height)));
+            score = 0;
+            timeLeft = 30; // 30-second countdown
+
+            walls = new Wall[]
+            {
+                new Wall(new Rectangle(100, 100, 600, 20)),
+                new Wall(new Rectangle(100, 200, 20, 300)),
+                new Wall(new Rectangle(300, 300, 400, 20)),
+                new Wall(new Rectangle(500, 400, 20, 100))
+            };
+
+            gameTimer = new System.Windows.Forms.Timer();
+            gameTimer.Interval = 100; // Update every 100 ms
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
+
+            countdownTimer = new System.Windows.Forms.Timer();
+            countdownTimer.Interval = 1000; // Update every 1 second
+            countdownTimer.Tick += CountdownTimer_Tick;
+            countdownTimer.Start();
+
+            this.Paint += MainForm_Paint;
+            this.KeyDown += MainForm_KeyDown;
+            this.KeyUp += MainForm_KeyUp;
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            playerWolf.UpdatePosition();
+            CheckCollision();
+            Invalidate(); // Triggers the Paint event
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+            }
+            else
+            {
+                gameTimer.Stop();
+                countdownTimer.Stop();
+                MessageBox.Show($"Time's up! Your score is {score}", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.CornflowerBlue);
+            playerWolf.Draw(g);
+            item.Draw(g);
+
+            foreach (var wall in walls)
+            {
+                wall.Draw(g);
+            }
+
+            // Draw score and time left
+            g.DrawString($"Score: {score}", new Font("Arial", 16), Brushes.White, new PointF(10, 10));
+            g.DrawString($"Time Left: {timeLeft} s", new Font("Arial", 16), Brushes.White, new PointF(10, 40));
+        }
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    playerWolf.SetVelocity(-10, 0);
+                    break;
+                case Keys.D:
+                    playerWolf.SetVelocity(10, 0);
+                    break;
+                case Keys.W:
+                    playerWolf.SetVelocity(0, -10);
+                    break;
+                case Keys.S:
+                    playerWolf.SetVelocity(0, 10);
+                    break;
+            }
+        }
+
+        private void MainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                case Keys.D:
+                case Keys.W:
+                case Keys.S:
+                    playerWolf.SetVelocity(0, 0);
+                    break;
+            }
+        }
+
+        private void CheckCollision()
+        {
+            Rectangle wolfRect = new Rectangle(playerWolf.Position.X, playerWolf.Position.Y, 50, 50);
+            Rectangle itemRect = new Rectangle(item.Position.X, item.Position.Y, 10, 10);
+
+            if (wolfRect.IntersectsWith(itemRect))
+            {
+                score++;
+                item = new Item(new Point(random.Next(ClientSize.Width), random.Next(ClientSize.Height)));
+            }
+
+            foreach (var wall in walls)
+            {
+                if (wolfRect.IntersectsWith(wall.Rectangle))
+                {
+                    // Reposition wolf to avoid collision
+                    playerWolf.SetVelocity(0, 0);
+                    if (playerWolf.Position.X < wall.Rectangle.Left)
+                    {
+                        playerWolf.Position = new Point(wall.Rectangle.Left - 50, playerWolf.Position.Y);
+                    }
+                    else if (playerWolf.Position.X + 50 > wall.Rectangle.Right)
+                    {
+                        playerWolf.Position = new Point(wall.Rectangle.Right, playerWolf.Position.Y);
+                    }
+                    else if (playerWolf.Position.Y < wall.Rectangle.Top)
+                    {
+                        playerWolf.Position = new Point(playerWolf.Position.X, wall.Rectangle.Top - 50);
+                    }
+                    else if (playerWolf.Position.Y + 50 > wall.Rectangle.Bottom)
+                    {
+                        playerWolf.Position = new Point(playerWolf.Position.X, wall.Rectangle.Bottom);
+                    }
+                }
+            }
+        }
+    }
+}
     public class Wolf
     {
         public string Name { get; set; }
         public int Age { get; set; }
         public Point Position { get; set; }
+        private Point velocity;
+
         public Wolf(string name, int age, Point startPosition)
         {
             Name = name;
@@ -18,7 +174,6 @@ namespace WolfGame
 
         public void Draw(Graphics g)
         {
-            // Drawing an improved wolf head
             Pen pen = new Pen(Color.Black, 2);
             Brush grayBrush = new SolidBrush(Color.Gray);
             Brush darkGrayBrush = new SolidBrush(Color.DarkGray);
@@ -69,169 +224,40 @@ namespace WolfGame
         {
             Position = new Point(Position.X + velocity.X, Position.Y + velocity.Y);
         }
-
-        private Point velocity;
     }
 
-    public class Item
+    // Continue with the Item and Wall classes...
+    namespace WolfGame
     {
-        public Point Position { get; set; }
-        public Item(Point position)
+        public class Item
         {
-            Position = position;
-        }
+            public Point Position { get; set; }
 
-        public void Draw(Graphics g)
-        {
-            Brush brush = new SolidBrush(Color.Gold);
-            g.FillEllipse(brush, Position.X, Position.Y, 10, 10);
-        }
-    }
-
-    public class Wall
-    {
-        public Rectangle Rectangle { get; set; }
-        public Wall(Rectangle rectangle)
-        {
-            Rectangle = rectangle;
-        }
-
-        public void Draw(Graphics g)
-        {
-            Brush brush = new SolidBrush(Color.Brown);
-            g.FillRectangle(brush, Rectangle);
-        }
-    }
-
-    public partial class MainForm : Form
-    {
-        private System.Windows.Forms.Timer gameTimer;
-        private Wolf playerWolf;
-        private Item item;
-        private Wall[] walls;
-        private Random random;
-        private int score;
-
-        public MainForm()
-        {
-            InitializeComponent();
-            this.DoubleBuffered = true;
-            this.ClientSize = new Size(800, 600);
-
-            playerWolf = new Wolf("Alpha", 30, new Point(400, 300));
-            random = new Random();
-            item = new Item(new Point(random.Next(ClientSize.Width), random.Next(ClientSize.Height)));
-            score = 0;
-
-            walls = new Wall[]
+            public Item(Point position)
             {
-                new Wall(new Rectangle(100, 100, 600, 20)),
-                new Wall(new Rectangle(100, 200, 20, 300)),
-                new Wall(new Rectangle(300, 300, 400, 20)),
-                new Wall(new Rectangle(500, 400, 20, 100))
-            };
-
-            gameTimer = new System.Windows.Forms.Timer();
-            gameTimer.Interval = 100; // Update every 100 ms
-            gameTimer.Tick += GameTimer_Tick;
-            gameTimer.Start();
-
-            this.Paint += MainForm_Paint;
-            this.KeyDown += MainForm_KeyDown;
-            this.KeyUp += MainForm_KeyUp;
-        }
-
-        private void GameTimer_Tick(object sender, EventArgs e)
-        {
-            // Update game logic here
-            playerWolf.UpdatePosition();
-            CheckCollision();
-            Invalidate(); // Triggers the Paint event
-        }
-
-        private void MainForm_Paint(object sender, PaintEventArgs e)
-        {
-            Graphics g = e.Graphics;
-            g.Clear(Color.CornflowerBlue);
-            playerWolf.Draw(g);
-            item.Draw(g);
-            foreach (var wall in walls)
-            {
-                wall.Draw(g);
+                Position = position;
             }
 
-            // Draw score
-            g.DrawString($"Score: {score}", new Font("Arial", 16), Brushes.White, new PointF(10, 10));
-        }
-
-        private void MainForm_KeyDown(object sender, KeyEventArgs e)
-        {
-            switch (e.KeyCode)
+            public void Draw(Graphics g)
             {
-                case Keys.A:
-                    playerWolf.SetVelocity(-10, 0);
-                    break;
-                case Keys.D:
-                    playerWolf.SetVelocity(10, 0);
-                    break;
-                case Keys.W:
-                    playerWolf.SetVelocity(0, -10);
-                    break;
-                case Keys.S:
-                    playerWolf.SetVelocity(0, 10);
-                    break;
+                Brush brush = new SolidBrush(Color.Gold);
+                g.FillEllipse(brush, Position.X, Position.Y, 10, 10);
             }
         }
 
-        private void MainForm_KeyUp(object sender, KeyEventArgs e)
+        public class Wall
         {
-            switch (e.KeyCode)
-            {
-                case Keys.A:
-                case Keys.D:
-                case Keys.W:
-                case Keys.S:
-                    playerWolf.SetVelocity(0, 0);
-                    break;
-            }
-        }
+            public Rectangle Rectangle { get; set; }
 
-        private void CheckCollision()
-        {
-            Rectangle wolfRect = new Rectangle(playerWolf.Position.X, playerWolf.Position.Y, 50, 50);
-            Rectangle itemRect = new Rectangle(item.Position.X, item.Position.Y, 10, 10);
-
-            if (wolfRect.IntersectsWith(itemRect))
+            public Wall(Rectangle rectangle)
             {
-                score++;
-                item = new Item(new Point(random.Next(ClientSize.Width), random.Next(ClientSize.Height)));
+                Rectangle = rectangle;
             }
 
-            foreach (var wall in walls)
+            public void Draw(Graphics g)
             {
-                if (wolfRect.IntersectsWith(wall.Rectangle))
-                {
-                    // Reposition wolf to avoid collision
-                    playerWolf.SetVelocity(0, 0);
-
-                    if (playerWolf.Position.X < wall.Rectangle.Left)
-                    {
-                        playerWolf.Position = new Point(wall.Rectangle.Left - 50, playerWolf.Position.Y);
-                    }
-                    else if (playerWolf.Position.X + 50 > wall.Rectangle.Right)
-                    {
-                        playerWolf.Position = new Point(wall.Rectangle.Right, playerWolf.Position.Y);
-                    }
-                    else if (playerWolf.Position.Y < wall.Rectangle.Top)
-                    {
-                        playerWolf.Position = new Point(playerWolf.Position.X, wall.Rectangle.Top - 50);
-                    }
-                    else if (playerWolf.Position.Y + 50 > wall.Rectangle.Bottom)
-                    {
-                        playerWolf.Position = new Point(playerWolf.Position.X, wall.Rectangle.Bottom);
-                    }
-                }
+                Brush brush = new SolidBrush(Color.Brown);
+                g.FillRectangle(brush, Rectangle);
             }
         }
     }
-}
